913b01df025f8979e2fec87f341a192a67b645ae
Mathias Legarreta <mathiaslegarreta18@hotmail.com>
2023-10-16 21:12:06 -0300

    tuki

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-let buttonClicked = document.getElementById("input-btn")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
-buttonClicked.addEventListener("click", function(){
-    console.log("button clicked");
-
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    console.log(myLeads );
 })
+    
+for (let i = 0; i < myLeads.length; i++){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
+    
+
+    
+
+
+
+ 
